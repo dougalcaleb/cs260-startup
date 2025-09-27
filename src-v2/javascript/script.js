5a54c812a2a5491be17de05318585bc63475e825
dougalcaleb <dougalcaleb@gmail.com>
2025-09-26 19:48:42 -0600

    Fancy header

--- src-v2/javascript/script.js
+++ src-v2/javascript/script.js
@@ -10,3 +10,26 @@ if (sessionStorage.getItem("has-loaded")) {
 }
 
 sessionStorage.setItem("has-loaded", true);
+
+// Mobile header title fade
+
+
+if (window.innerWidth < 900) {
+	const header = document.querySelector("header");
+	const bigHeader = document.querySelector("#pre-header-logo");
+	const smallHeader = document.querySelector("#header-logo-wrap");
+	const textHeight = bigHeader.getBoundingClientRect().height;
+	const headerBound = header.getBoundingClientRect();
+
+	bigHeader.style.top = `${headerBound.bottom / 2 - textHeight / 2}px`;
+	smallHeader.style.opacity = 0;
+
+	window.addEventListener("scroll", (event) => {
+		const headerBound = header.getBoundingClientRect();
+		const percentScrolled = (window.scrollY / (window.innerHeight / 5));
+
+		bigHeader.style.top = `${headerBound.bottom / 2 - textHeight / 2}px`;
+		bigHeader.style.opacity = 1 - percentScrolled * 3;
+		smallHeader.style.opacity = (percentScrolled - 0.3) * 3;
+	});	
+}
