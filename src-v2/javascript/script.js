eba74720c88dea129cddd0da9175495e59244413
dougalcaleb <dougalcaleb@gmail.com>
2025-09-30 13:19:39 -0600

    Added login/credit page

--- src-v2/javascript/script.js
+++ src-v2/javascript/script.js
@@ -1,16 +1,3 @@
-document.querySelector(".load-screen").style.display = "none";
-
-if (sessionStorage.getItem("has-loaded")) {
-	document.querySelector(".load-screen").style.display = "none";
-} else {
-	window.onload = () => {
-		document.querySelector(".load-screen p").style.animation = "0.5s ease-in forwards clip-in";
-		document.querySelector(".load-screen").style.animation = "0.2s 2s linear forwards fadeout";
-	};
-}
-
-sessionStorage.setItem("has-loaded", true);
-
 // Mobile header title fade
 
 const header = document.querySelector("header");
---
+++ src-v2/javascript/splash.js
@@ -0,0 +1,12 @@
+document.querySelector(".load-screen").style.display = "none";
+
+if (sessionStorage.getItem("has-loaded")) {
+	document.querySelector(".load-screen").style.display = "none";
+} else {
+	window.onload = () => {
+		document.querySelector(".load-screen p").style.animation = "0.5s ease-in forwards clip-in";
+		document.querySelector(".load-screen").style.animation = "0.2s 2s linear forwards fadeout";
+	};
+}
+
+sessionStorage.setItem("has-loaded", true);
