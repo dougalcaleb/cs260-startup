ddd8de41e9fd9610a6c4c86b6f239bfaa1016c6f
Caleb <dougalcaleb@gmail.com>
2025-10-06 22:31:38 -0600

    Routes to all pages

--- src/parallel/src/main.jsx
+++ src/parallel/src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/pages/login'
+import Library from './components/pages/library'
+import Nearby from './components/pages/nearby'
+import Search from './components/pages/search'
+import Connect from './components/pages/connect'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/library" element={<Library />} />
+				<Route path="/nearby" element={<Nearby />} />
+				<Route path="/search" element={<Search />} />
+				<Route path="/connect" element={<Connect />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 )
