b3b2dfada37ef9250a3bff48b5b5eccae0dfeebb
dougalcaleb <dougalcaleb@gmail.com>
2025-10-08 23:28:41 -0600

    Library page images

--- src/parallel/src/components/pages/library.jsx
+++ src/parallel/src/components/pages/library.jsx
@@ -1,10 +1,24 @@
-import Header from "../shared/header";
+import Header from "../shared/header"
 import Footer from "../shared/footer"
+import phimg from "../../assets/landscape-200.jpg"
 
 export default function Library() {
 	return (
-		<div className="bg-gray-1 w-full min-h-[175vh] sm:min-h-[calc(100vh - max(7vh, 70px))]">
-
+		<div className="bg-gray-1 w-full min-h-[175vh] sm:min-h-[calc(100vh-max(7vh,70px))]">
+			<div className="font-main font-black text-gray-6 text-center sm:text-left w-full sm:w-auto pt-4 sm:pt-6 text-2xl sm:ml-8">LIBRARY</div>
+			<div className="grid p-4 gap-2.5" id="library-content">
+				{ images() }
+			</div>
 		</div>
 	);
+}
+
+function images() {
+	const arr = [];
+
+	for (let i = 0; i < 20; i++) {
+		arr.push(<img key={i} src={phimg} alt={`placeholder ${i}`} className="w-full h-auto object-cover rounded-md" />);
+	}
+
+	return arr;
 }
--- src/parallel/src/main.jsx
+++ src/parallel/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './additional.css'
 import Root from './components/root'
 import { BrowserRouter } from 'react-router-dom'
 
