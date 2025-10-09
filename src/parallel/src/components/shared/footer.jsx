8685d324e3be66aae507436dc56ad409c39edf20
dougalcaleb <dougalcaleb@gmail.com>
2025-10-08 23:59:16 -0600

    Fix nav text color

--- src/parallel/src/components/shared/footer.jsx
+++ src/parallel/src/components/shared/footer.jsx
@@ -13,14 +13,14 @@ export default function Footer() {
 	return (
 		<div className="fixed rounded-tr-4xl rounded-tl-4xl h-20 w-full bottom-0 left-0 flex justify-center items-center bg-gray-3 sm:hidden -mb-4">
 			<nav className="bg-gray-1 w-[80%] h-6 rounded-4xl flex justify-between items-center">
-				<Link to={!atLibrary ? '/library' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atLibrary ? selectedClass : ''}`}>
-					<div className="w-full text-white-0 font-main font-black text-center flex justify-center text-xs">YOU</div>
+				<Link to={!atLibrary ? '/library' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atLibrary ? selectedClass : 'text-white-0'}`}>
+					<div className="w-full font-main font-black text-center flex justify-center text-xs">YOU</div>
 				</Link>
-				<Link to={!atNearby ? '/nearby' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atNearby ? selectedClass : ''}`}>
-					<div className="w-full text-white-0 font-main font-black text-center flex justify-center text-xs">NEARBY</div>
+				<Link to={!atNearby ? '/nearby' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atNearby ? selectedClass : 'text-white-0'}`}>
+					<div className="w-full font-main font-black text-center flex justify-center text-xs">NEARBY</div>
 				</Link>
-				<Link to={!atSearch ? '/search' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atSearch ? selectedClass : ''}`}>
-					<div className="w-full text-white-0 font-main font-black text-center flex justify-center text-xs">SEARCH</div>
+				<Link to={!atSearch ? '/search' : null} className={`w-[30%] h-full flex flex-col justify-center rounded-4xl ${atSearch ? selectedClass : 'text-white-0'}`}>
+					<div className="w-full font-main font-black text-center flex justify-center text-xs">SEARCH</div>
 				</Link>
 			</nav>
 		</div>
