5c58145697c5cf34b279ea3b14f072786476b6a3
dougalcaleb <dougalcaleb@gmail.com>
2025-11-13 19:33:54 -0700

    Fix minor file picker visual issues

--- src/parallel/src/components/shared/FilePicker.jsx
+++ src/parallel/src/components/shared/FilePicker.jsx
@@ -40,8 +40,10 @@ export default function FilePicker({ id, multiple = false, onChange = () => {}, 
 						<div className="flex flex-col mb-4">
 							{files.map((f, i) => (
 								<div key={`file-display-${inputId}-${i}`} className="font-mono text-gray-8 hover:bg-gray-3 p-1 flex items-center">
-									<p className="break-all">{f.name} ({formatFileSize(f.size)})</p>
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="h-8 w-8 sm:w-6 sm:h-6 ml-2 cursor-pointer text-gray-7 hover:text-red-2" onClick={() => removeFile(i)}>
+									<p >
+										<span className="break-all">{f.name}</span> ({formatFileSize(f.size)})
+									</p>
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="min-h-5 min-w-5 w-5 h-5 sm:w-5 sm:h-5 ml-2 cursor-pointer text-gray-7 hover:text-red-2" onClick={() => removeFile(i)}>
 										<path fill="currentColor" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z" />
 									</svg>
 								</div>
